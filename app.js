ccfaac2340bc424d4dd96c761746ad3a719b1cc4
Rafal <rafalp443@gmail.com>
2022-05-31 13:04:00 +0200

    Create basic layout for calculator
    Created simple html layout for the calculator using css
    grid-template-areas property. It contains buttons for numbers and
    operational input. I also create basic display at the top of the
    calculator.

---
+++ app.js
@@ -0,0 +1,19 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const substract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function(operator, a, b) {
+    return operator(a, b);
+}
