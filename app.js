368657c1179c8a7dd68e15435e962c0cce304d64
Rafal <rafalp443@gmail.com>
2022-06-03 13:22:02 +0200

    Add basic functionality to hold one operation
    User can choose first number, operator and second number to get the
    result of single operation. Every value is shown on the display.
     I also implemented function for 'AC' button to clear all the data
     from variables and from display.

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+const displayNode = document.querySelector('#display');
+let displayValue = '';
+let firstNumber;
+let secondNumber;
+let operator;
+let result;
+
+const digitButtons = document.querySelectorAll('.digit');
+digitButtons.forEach(btn => btn.addEventListener('click', inputDigit));
+
+const operationButtons = document.querySelectorAll('.operator');
+operationButtons.forEach(btn => btn.addEventListener('click', inputOperation));
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', getResult);
+
+const allclearButton = document.querySelector('#allclear');
+allclearButton.addEventListener('click', clearAll);
+
+
+
+function updateDisplay() {
+    displayNode.textContent = displayValue;
+}
+
+function inputDigit(e) {
+    displayValue += e.target.innerText;
+    updateDisplay();
+}
+
+function inputOperation(e) {
+    const id = e.target.id;
+
+    if (!firstNumber) {
+        firstNumber = parseInt(displayValue);
+        displayValue = ''; 
+    }
+    
+    switch (id) {
+        case 'add':
+            operator = add;
+            break;
+        case 'substract':
+            operator = substract;
+            break;
+        case 'multiply':
+            operator = multiply;
+            break;
+        case 'divide':
+            operator = divide;
+            break;
+    }
+}
+
+function getResult() {
+    if (firstNumber && operator) {
+        secondNumber = parseInt(displayValue);
+        result = operate(operator, firstNumber, secondNumber);
+        displayValue = result;
+        updateDisplay();
+    }
+}
+
+function clearAll() {
+    displayValue = '';
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    result = undefined;
+    updateDisplay();
+}
+
 const add = function(a, b) {
     return a + b;
 }
